a2abf974bdf65341553141cb93ba89baab09a4ae
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2025-12-23 13:54:59 +0100

    chore: update compiler version

--- cmake/toolchain/stm32f411.cmake
+++ cmake/toolchain/stm32f411.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER  /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb)
-set(CMAKE_CXX_COMPILER /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb)
+set(CMAKE_C_COMPILER  /opt/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb)
+set(CMAKE_CXX_COMPILER /opt/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb)
 
 set(CMAKE_C_FLAGS_INIT -ffreestanding)
 set(CMAKE_CXX_FLAGS_INIT -ffreestanding)
